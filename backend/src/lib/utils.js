27b2c1f541e19e2fcd6099bf4554a91e2ba1a3b8
BenedictGutierrez <bennyshark01@gmail.com>
2025-09-17 10:06:24 +0800

    update profile endpoint added

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,14 +8,14 @@ export const generateToken = (userId, res) => {
   }
 
   const token = jwt.sign({ userId }, JWT_SECRET, {
-    expiresIn: "7d",
+    expiresIn: "7d", // server-side token expiry (token becomes invalid).
   });
 
   res.cookie("jwt", token, {
-      maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-      httpOnly: true, // prevent XSS attactks: cross-site scripting
-      sameSite: "strict", // CSRF attacks
-      secure: ENV.NODE_ENV === "development" ? false : true
+    maxAge: 7 * 24 * 60 * 60 * 1000, // MS client-side cookie lifetime (browser deletes cookie).
+    httpOnly: true, // prevent XSS attactks: cross-site scripting
+    sameSite: "strict", // CSRF attacks
+    secure: ENV.NODE_ENV === "development" ? false : true,
   });
 
     return token;
