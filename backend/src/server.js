5c5512d1dd2b160e16e7f6fc219cf2a1d70e8409
BenedictGutierrez <bennyshark01@gmail.com>
2025-09-23 11:56:13 +0800

    aignu[ frontend implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/messages.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import cors from "cors";
 
 const app = express();
 const __dirname = path.resolve();
@@ -14,6 +15,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json()) // req.body
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true})) //allows frontend to send cookie to our backend
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
